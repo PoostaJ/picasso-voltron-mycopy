69e068fe22c9df8efdbcdf9d9757a65f1da5a693
Fumarola <fumarolad23@mail.wlu.edu>
2021-12-02 23:20:47 -0500

    made analyzer for cos

---
+++ src/picasso/parser/CosAnalyzer.java
@@ -0,0 +1,25 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Cos;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the Cosine function
+ * 
+ * @author Dario Fumarola
+ */
+
+public class CosAnalyzer extends UnaryFunctionAnalyzer {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop();
+		
+		return new Cos(SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens));
+	}
+
+}
