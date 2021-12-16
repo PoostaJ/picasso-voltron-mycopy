7bb15b6f093aa3a8dbcf91d219b8b3b87f89c7bb
p-stoffel <stoffelp23@mail.wlu.edu>
2021-12-15 19:54:48 -0500

    Added the perlinColor function - still needs testing

---
+++ src/picasso/parser/tokens/functions/PerlinColorToken.java
@@ -0,0 +1,21 @@
+package picasso.parser.tokens.functions;
+
+
+/**
+ * Represents the PerlinColor function token
+ * 
+ * @author Patrick Stoffel 
+ *
+ */
+public class PerlinColorToken extends FunctionToken {
+
+	
+	/**
+	 * PerlinColorToken constructor
+	 *
+	 */
+	public PerlinColorToken() {
+		super("PerlinColor Function Token");
+	}
+
+}
